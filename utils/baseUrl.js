591bd1cf599b24061d82e04b840c333dcfe2fe24
Ahmad Amin <baigahmad323@gmail.com>
2024-01-18 18:12:27 +0500

    Email Template Fixed

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
     ? "https://snapcreditsolutions.com"
-    : "http://localhost:3002";
+    : "http://localhost:3001";
 
 export default baseUrl;
