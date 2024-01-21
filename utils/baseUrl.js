f39e06fd8b28d6d6bd29d4b630c032d2f5782e4a
Ahmad Amin <baigahmad323@gmail.com>
2024-01-21 19:23:45 +0500

    Website URL Changed for testing purpose

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://snapcreditsolutions.com"
+    ? "https://main--admirable-treacle-014e7f.netlify.app/"
     : "http://localhost:3001";
 
 export default baseUrl;
